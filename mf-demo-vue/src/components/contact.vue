5018b527b9b1e0f9678897fce2b0e66df65441de
anetastankovska <aneta.stankovska@sourcico.com>
2023-05-12 00:56:00 +0200

    add contact

---
+++ mf-demo-vue/src/components/contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- Your component template goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  // Your component logic goes here
+}
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
